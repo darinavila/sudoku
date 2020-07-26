28d37a3d41a7138556a493a80e5f539124de9406
darinavila <67657508+darinavila@users.noreply.github.com>
2020-07-26 18:42:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -103,77 +103,78 @@ while True:
             for x in range(0,9):
                 
                 squares[a] = warp[height*y+offset_height:height*(y+1), width*x+offset_width:width*(x+1)-offset_width]    
-                if squares[a][:,0:3].any() is not 255:
-                    squares[a] = warp[height*y+offset_height:height*(y+1)+offset_height, width*x+round(1.5*offset_width):width*(x+1)-offset_width]    
-                
-                if y >= 3 and x>= 3:   
-                    height_rn = height*y+round(2*offset_height)
-                    squares[a] = warp[height*y+round(2*offset_height):height*(y+1)+round(2*offset_height), width*x+round(2.5*offset_width):width*(x+1)-offset_width]              
-                    if squares[a][0:3,:].any() is not 255:
-                        squares[a] = warp[height_rn + 5:height*(y+1), width*x+offset_width:width*(x+1)-offset_width]    
-                
-                
-                ratio = 0.6      
-             
-                while np.sum(squares[a][0]) <= (1-ratio) * squares[a].shape[1] * 255:
-                    squares[a] = squares[a][1:]
-                
-                while np.sum(squares[a][:,-1]) <= (1-ratio) * squares[a].shape[1] * 255:
-                    squares[a] = np.delete(squares[a], -1, 1)
-              
-                while np.sum(squares[a][:,0]) <= (1-ratio) * squares[a].shape[0] * 255:
-                    squares[a] = np.delete(squares[a], 0, 1)
-                
-                while np.sum(squares[a][-1]) <= (1-ratio) * squares[a].shape[0] * 255:
-                    squares[a] = squares[a][:-1]    
-               
-                squares[a] = cv2.resize(squares[a], (28,28), interpolation = cv2.INTER_CUBIC)
-                center_width = squares[a].shape[1] // 2
-                center_height = squares[a].shape[0] // 2
-                x_start = center_height // 2
-                x_end = center_height // 2 + center_height
-                y_start = center_width // 2
-                y_end = center_width // 2 + center_width
-                center_region = squares[a][x_start:x_end, y_start:y_end]
-                if center_region.sum() >= center_width * center_height * 255 - 255:
-                    squares[a] = None
-                else:
-                    squares[a][0:4,0:4] = 255
-                    image, contours, hierarchy = cv2.findContours(squares[a], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                if squares[a] is not None:
+                    if squares[a][:,0:3].any() is not 255:
+                        squares[a] = warp[height*y+offset_height:height*(y+1)+offset_height, width*x+round(1.5*offset_width):width*(x+1)-offset_width]    
                     
-                    maxA = -1
-                    maxC = None
-    
-                    for f in contours:
-                        if cv2.contourArea(f) > maxA:
-                            maxA = cv2.contourArea(f)
-                            maxC = f
-
-                    epsilon = 0.025*cv2.arcLength(maxC, True)
-                    poly = cv2.approxPolyDP(maxC, epsilon, True)
-                    rect = cv2.minAreaRect(poly)
+                    if y >= 3 and x>= 3:   
+                        height_rn = height*y+round(2*offset_height)
+                        squares[a] = warp[height*y+round(2*offset_height):height*(y+1)+round(2*offset_height), width*x+round(2.5*offset_width):width*(x+1)-offset_width]              
+                        if squares[a][0:3,:].any() is not 255:
+                            squares[a] = warp[height_rn + 5:height*(y+1), width*x+offset_width:width*(x+1)-offset_width]    
                     
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    pts = box
+                    
+                    ratio = 0.6      
                  
+                    while np.sum(squares[a][0]) <= (1-ratio) * squares[a].shape[1] * 255:
+                        squares[a] = squares[a][1:]
                     
-                    maxx = box[0][1] 
-                    minx = box[2][1] 
-                    maxy = box[0][0] 
-                    miny = box[1][0] 
-                   
+                    while np.sum(squares[a][:,-1]) <= (1-ratio) * squares[a].shape[1] * 255:
+                        squares[a] = np.delete(squares[a], -1, 1)
+                  
+                    while np.sum(squares[a][:,0]) <= (1-ratio) * squares[a].shape[0] * 255:
+                        squares[a] = np.delete(squares[a], 0, 1)
                     
+                    while np.sum(squares[a][-1]) <= (1-ratio) * squares[a].shape[0] * 255:
+                        squares[a] = squares[a][:-1]    
                    
-                    croped = squares[a][miny:maxy+2, minx:maxx+2]
+                    squares[a] = cv2.resize(squares[a], (28,28), interpolation = cv2.INTER_CUBIC)
+                    center_width = squares[a].shape[1] // 2
+                    center_height = squares[a].shape[0] // 2
+                    x_start = center_height // 2
+                    x_end = center_height // 2 + center_height
+                    y_start = center_width // 2
+                    y_end = center_width // 2 + center_width
+                    center_region = squares[a][x_start:x_end, y_start:y_end]
+                    if center_region.sum() >= center_width * center_height * 255 - 255:
+                        squares[a] = None
+                    else:
+                        squares[a][0:4,0:4] = 255
+                        image, contours, hierarchy = cv2.findContours(squares[a], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                        
+                        maxA = -1
+                        maxC = None
+        
+                        for f in contours:
+                            if cv2.contourArea(f) > maxA:
+                                maxA = cv2.contourArea(f)
+                                maxC = f
 
-                   
-                    if croped.shape[0] > 0 and croped.shape[1] > 0:
-                        squares[a] = croped
-                        squares[a] = cv2.resize(squares[a], (28,28), interpolation = cv2.INTER_CUBIC)
+                        epsilon = 0.025*cv2.arcLength(maxC, True)
+                        poly = cv2.approxPolyDP(maxC, epsilon, True)
+                        rect = cv2.minAreaRect(poly)
+                        
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
+                        pts = box
+                     
+                        
+                        maxx = box[0][1] 
+                        minx = box[2][1] 
+                        maxy = box[0][0] 
+                        miny = box[1][0] 
+                       
+                        
+                       
+                        croped = squares[a][miny:maxy+2, minx:maxx+2]
 
-                    
-                a+=1            
+                       
+                        if croped.shape[0] > 0 and croped.shape[1] > 0:
+                            squares[a] = croped
+                            squares[a] = cv2.resize(squares[a], (28,28), interpolation = cv2.INTER_CUBIC)
+
+                        
+                    a+=1            
 
        
         check, frame2 = video.read()
